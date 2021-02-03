27d13d5a11b2474a33497b980fa91a12d9324a5d
Andrey <erkcasb@gmail.com>
2021-02-03 10:39:15 +0500

    code formatted

--- src/test/java/task1test/IntsCalculatorTest.java
+++ src/test/java/task1test/IntsCalculatorTest.java
@@ -7,20 +7,20 @@ import task1.IntsCalculator;
 
 public class IntsCalculatorTest {
     @Test
-    public void getCalculation_validArgument_success(){
+    public void getCalculation_validArgument_success() {
         //given
         Ints intsCalc = new IntsCalculator();
-        final int argA=5;
-        final int argB=10;
+        final int argA = 5;
+        final int argB = 10;
 
         final int expSum = 15;
         final int expMult = 50;
         final int expPow = 9765625;
 
         //when
-        final int resultSum = intsCalc.sum(argA,argB);
-        final int resultMult = intsCalc.mult(argA,argB);
-        final int resultPow = intsCalc.pow(argA,argB);
+        final int resultSum = intsCalc.sum(argA, argB);
+        final int resultMult = intsCalc.mult(argA, argB);
+        final int resultPow = intsCalc.pow(argA, argB);
 
         //then
         Assertions.assertEquals(expSum, resultSum);
